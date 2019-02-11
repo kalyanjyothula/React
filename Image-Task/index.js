badb19319e1b3aa9cf483e41c673fbac8ae91aab
KalyanJyothula <38788245+kalyanjyothula@users.noreply.github.com>
2019-02-11 16:14:34 +0530

    comments added

--- Image-Task/index.js
+++ Image-Task/index.js
@@ -10,9 +10,9 @@ class App extends React.Component{
         //api access
         const response = await RequestApi.get('search/photos',{
             params:{query:term},
-            headers:{
+           /* headers:{
                 Authorization: 'Client-ID 3a942e75b3cdd850b7c8806aca57b950323474f6a684e5cb0040ba65344ee987'
-            }
+            }*/
         });
         this.setState({images:response.data.results});
     }
